14e345e6422ab0d45fab813d47fcfd5d3d76466d
oluizantoniofilho <oluizantoniofilho@gmail.com>
2025-11-16 06:42:19 +0000

    atualização da linha importação no arquivo src app pagetsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import dynamic from 'next/dynamic';
-import Hero from '@/components/home/Hero';
+import { Hero } from '@/components/home/Hero';
 import { CamaraHeroCTA } from "@/components/home/CamaraHeroCTA";
 import { ManifestoSection } from "@/components/home/ManifestoSection";
 
